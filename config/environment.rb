4b61de3622a4a7f4be50c02a05637967cb6685b8
ajospino <ajospinoa@eafit.edu.co>
2023-04-27 19:16:22 -0500

    Debugging secret_key_base error

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require_relative "application"
-
+Dotenv::Railtie.load
 # Initialize the Rails application.
 Rails.application.configure do config.secret_key_base = ENV["SECRET_KEY_BASE"] end
 Rails.application.initialize!
