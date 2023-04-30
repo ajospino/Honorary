bf045da0cf56cf3e3fed59725752f7d4c9552dcb
ajospino <ajospinoa@eafit.edu.co>
2023-04-30 01:40:24 -0500

    Debugging db error

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,16 @@ require_relative "boot"
 
 require "rails/all"
 
+require 'erb'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Honorary
   class Application < Rails::Application
-    Dotenv::Railtie.load
+    
+    ActiveRecord::Base.establish_connection( YAML.load( ERB.new( File.read( "#{app_dir}/config/database.yml" )).result)[rails_env])
     # Initialize configuration defaults for originally generated Rails version.
     Rails.application.config.hosts << "honoraryprod.us-east-2.elasticbeanstalk.com"
     config.load_defaults 7.0
